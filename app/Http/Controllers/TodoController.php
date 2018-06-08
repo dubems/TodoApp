5d5f96637f7de721e8118e6b80cc682b3031b346
Dubems <nriagudubem@yahoo.com>
2018-06-08 19:35:09 +0100

    my first commit

---
+++ app/Http/Controllers/TodoController.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Repository\TodoRepository;
+use Illuminate\Http\Request;
+
+class TodoController extends Controller
+{
+    /**
+     *
+     */
+    private $todoRepository;
+
+    /**
+     * @param TodoRepository $todoRepository
+     */
+    public function __construct(TodoRepository $todoRepository)
+    {
+        $this->todoRepository = $todoRepository;
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return response()->json($this->todoRepository->getAll());
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        return $this->todoRepository->create($this->formatRequest($request));
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return response()->json($this->todoRepository->read($id));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        return response()->json($this->todoRepository->read($id));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param  int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        return $this->todoRepository->update($this->formatRequest($request), $id);
+    }
+
+    /**
+     * @param Request $request
+     * @return mixed
+     */
+    private function formatRequest(Request $request)
+    {
+        $data['body'] = $request->get('body') ? $request->get('body') : null;
+        $data['title'] = $request->get('title') ? $request->get('title') : null;
+
+        return $data;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        return $this->todoRepository->delete($id);
+    }
+}
---
+++ app/Repository/TodoRepository.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Repository;
+
+use App\Todo;
+
+class TodoRepository
+{
+    /**
+     *
+     */
+    private $todo;
+
+    /**
+     * @param Todo $todo
+     */
+    public function __construct(Todo $todo)
+    {
+        $this->todo = $todo;
+    }
+
+    /**
+     * Return all Todo's
+     */
+    public function getAll()
+    {
+        return $this->todo->all();
+    }
+
+    /**
+     * Return a single Todo
+     * @param $id
+     */
+    public function read($id)
+    {
+        return $this->todo->where('id',$id)->first();
+    }
+
+    /** Creat e todo
+     * @param array $data
+     * @return bool
+     */
+    public function create(array $data)
+    {
+        $todo        = new Todo();
+        $todo->title = $data['title'];
+        $todo->body  = $data['body'];
+        $todo->save();
+
+        return $todo;
+    }
+
+    /** Update a todo
+     * @param array $data
+     * @param $id
+     * @return
+     */
+    public function update(array $data,$id)
+    {
+        $todo         = $this->todo->where('id',$id)->first();
+        $todo->body   = $data['body'] ? $data['body'] : $todo->body;
+        $todo->title  = $data['title'] ? $data['title'] : $todo->title;
+        $todo->save();
+
+        return $todo;
+    }
+
+    /** Delete a todo
+     * @param $id
+     * @return
+     */
+    public function delete($id)
+    {
+       return  $this->todo->where('id',$id)->delete();
+    }
+}
